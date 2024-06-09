96ad33a21bed7a4c40793a12d16206d41ea5959a
Kehinde Popoola <162780889+lumiworldprintz@users.noreply.github.com>
2024-06-08 19:57:24 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/* ==========  typing animation============*/
+var typed = new Typed(".typing",{
+    strings:["","web Designer", "UI/UX Designer", "Graphics Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})  
